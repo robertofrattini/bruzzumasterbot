593a6a064010cd8a33c63d59711a7720542c03e8
robertofrattini <41764268+robertofrattini@users.noreply.github.com>
2018-11-29 01:02:05 +0100

    L0

--- bot.php
+++ bot.php
@@ -1,18 +1,60 @@
 <?php
-
 define('API_URL', 'https://api.telegram.org/bot364944422:AAGd1iM_wwBqDEg119yUgtN-83y9zrVxJJU/');
 $update = json_decode(file_get_contents('php://input'), true);
 $chatId = $update['message']['chat']['id'];
 $params = [
-  'method' => "sendMessage",
   'chat_id' => $chatId,
   'text' => "Ciao",
-]
-$ch = curl_init(API_URL);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-  $res = json_decode(curl_exec($handle),true);
-curl_close($handle);
-
-?>
+];
+function exec_curl_request($handle) {
+    $response = curl_exec($handle);
+    if ($response === false) {
+      $errno = curl_errno($handle);
+      $error = curl_error($handle);
+      error_log("Curl returned error $errno: $error\n");
+      curl_close($handle);
+      return false;
+    }
+    $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+    curl_close($handle);
+    if ($http_code >= 500) {
+      sleep(10);
+      return false;
+    } else if ($http_code != 200) {
+      $response = json_decode($response, true);
+      error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+      if ($http_code == 401) {
+        throw new Exception('Invalid access token provided');
+      }
+      return false;
+    } else {
+      $response = json_decode($response, true);
+      if (isset($response['description'])) {
+        error_log("Request was successfull: {$response['description']}\n");
+      }
+      $response = $response['result'];
+    }
+    return $response;
+  }
+function apiRequestJson($method, $parameters) {
+    if (!is_string($method)) {
+      error_log("Method name must be a string\n");
+      return false;
+    }
+    if (!$parameters) {
+      $parameters = array();
+    }
+    else if (!is_array($parameters)) {
+      error_log("Parameters must be an array\n");
+      return false;
+    }
+    $parameters["method"] = $method;
+    $handle = curl_init(API_URL);
+      curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+      curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+      curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parameters));
+      curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+    return exec_curl_request($handle);
+  }
+apiRequestJson('sendMessage',$params);
